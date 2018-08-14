0e00b89f69159ab1247b0e634889a700379fd686
zhuozong_yao <zhuozong_yao@kingdee.com>
2018-08-14 15:49:02 +0800

    first commit

--- nettytest/src/main/java/com/test/netty/base/chapter3/TimeServerHandler.java
+++ nettytest/src/main/java/com/test/netty/base/chapter3/TimeServerHandler.java
@@ -1,4 +1,22 @@
 package com.test.netty.base.chapter3;
 
-public class TimeServerHandler {
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+public class TimeServerHandler extends ChannelHandlerAdapter {
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        // ctx.write()
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
+    }
 }
